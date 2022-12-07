a23ed5579ffcac3dfbca06c96dca6252f2aeff98
Nataliia Svystula <natashasvistula@gmail.com>
2022-12-07 14:31:11 +0200

    add

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+
+import { React, Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Searchbar } from './Searchbar/Searchbar';
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+        <Searchbar
+          onSubmit={this.searchImages}
+          toastInfo={this.toastInfoNothing}
+        />
+        <ToastContainer autoClose={3000} />
+      </>
+    );
+  }
+  
 };
+
+
+// {/* <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div> */}
