a6cbe56c9608721a9bf3409f7b68e01442a08f42
Nataliia Svystula <natashasvistula@gmail.com>
2022-12-08 15:08:54 +0200

    add

--- src/components/Louder/Louder.js
+++ src/components/Louder/Louder.js
@@ -3,14 +3,21 @@ import css from './Louder.module.css';
 
 export const Louder = () => (
   <div className={css.Loader}>
-    <Vortex 
-            visible={true}
-            height="80"
-            width="80"
-            ariaLabel="vortex-loading"
-            wrapperStyle={{}}
-            wrapperClass="vortex-wrapper"
-            colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
-          />
+    <Vortex
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="vortex-loading"
+      wrapperStyle={{}}
+      wrapperClass="vortex-wrapper"
+      colors={[
+        '#42ddf5',
+        '#4281f5',
+        '#5442f5',
+        '#8d42f5',
+        '#bf42f5',
+        '#f9f7fa',
+      ]}
+    />
   </div>
 );
